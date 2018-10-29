905a2f680c0ddc19b4f5e0b5a11cb579b223e6df
lvxin <562331245@qq.com>
2018-10-29 15:27:12 +0800

    test

--- src/test/java/com/lvxin/elasticsearchdemo/Service/CompoundQueriesImplTest.java
+++ src/test/java/com/lvxin/elasticsearchdemo/Service/CompoundQueriesImplTest.java
@@ -1,23 +1,23 @@
-package com.lvxin.elasticsearchdemo.Service;
-
-import com.lvxin.elasticsearchdemo.Service.impl.CompoundQueriesImpl;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-/**
- * Created by lvxin
- */
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class CompoundQueriesImplTest {
-    @Autowired
-    CompoundQueriesImpl compoundQueriesImpl;
-
-    @Test
-    public void test(){
-        compoundQueriesImpl.boolQuery("content","resolution");
-    }
-}
+//package com.lvxin.elasticsearchdemo.Service;
+//
+//import com.lvxin.elasticsearchdemo.Service.impl.CompoundQueriesImpl;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+///**
+// * Created by lvxin
+// */
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class CompoundQueriesImplTest {
+//    @Autowired
+//    CompoundQueriesImpl compoundQueriesImpl;
+//
+//    @Test
+//    public void test(){
+//        compoundQueriesImpl.boolQuery("content","resolution");
+//    }
+//}
